5e76308d0f167cc059ffb8367d45da49cd584c4f
Briiqn <82249811+Briiqn@users.noreply.github.com>
2023-11-11 12:01:45 -0500

    Add files via upload

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 cp usr/bin/* /usr/bin/
+cp sysd/targetwants/warp-svc.service /etc/systemd/system/multi-user.target.wants/
+cp sysd/warp-svc.service /usr/lib/systemd/system/warp-svc.service
 chmod +x /usr/bin/warp*
+
 warp-svc & disown
 echo $(pidof warp-svc)
 warp-cli register
