a4810a0809904a688459c6b0834b74d757f7e94f
Briiqn <82249811+Briiqn@users.noreply.github.com>
2023-11-11 14:11:17 -0500

    Add files via upload

--- init.sh
+++ init.sh
@@ -1,10 +1,6 @@
 cp wgcf /usr/bin/
-cp libhg.so.1.0.0 /usr/lib/
 chmod +rwx /usr/bin/wgcf
-wgcf register --accept-tos
-wgcf generate
-sed -i '4s/.*//' "wgcf-profile.conf"
-cat wgcf-profile.conf
-wg-quick up wgcf-profile.conf
-honeygain 
+chmod +x docker.sh
+./docker.sh
 ls
+
